8008530a5d5f48e579cad7c5a3a6757148fc88b8
Khris <khristianmarcial@gmail.com>
2019-11-30 01:51:35 -0500

    created list

--- js/script.js
+++ js/script.js
@@ -9,7 +9,22 @@ $('form').on('submit', (e)=>{
         url:'https://pokeapi.co/api/v2/pokemon/' + userInput.toLowerCase() + '/'
     }).then((data) => {
     
-    $('#stat').html(data.moves[0].move.name);
+
+        //check for undefined
+        //if undefined, do not append
+        //append an item to list
+        let pokeImage = "'"+ data.sprites.front_default + "'";
+        $('#picture').prepend("<img src="+ pokeImage +"></img>")
+
+
+    $('.number').html("#"+data.id);
+    $('.name').html(data.name);
+    $('.type1').html(data.types[0].type.name);
+    $('.type2').html(data.types[1].type.name);
+    $('.move1').html(data.moves[0].move.name);
+    $('.move2').html(data.moves[1].move.name);
+    $('.move3').html(data.moves[2].move.name);
+    $('.move4').html(data.moves[3].move.name);
     },
     (error) => {
     console.log('bad request: ', error);
