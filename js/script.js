8c915fb13e379ae1a741762b4004dbf25ad25054
Khris <khristianmarcial@gmail.com>
2019-12-03 22:49:16 -0500

    fixed some bugs

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 $('form').on('submit', (e)=>{
     e.preventDefault();
-    console.log(e.target);
+
     const userInput = $('input[type="text"]').val();
-    // const iconurls = 
-    console.log(userInput);
+
+    const addPokeInfo = (info) =>{
+        let ul = document.getElementById("pokemon");
+        let li = document.createElement("li");
+        li.setAttribute('class','list-group-item');
+        li.appendChild(document.createTextNode(info));
+        ul.appendChild(li);
+    }
+
+    const addMoves = (move) => {
+        let ul = document.getElementById("moveset");
+        let li = document.createElement("li");
+        li.setAttribute('class','list-group-item');
+        li.appendChild(document.createTextNode(move));
+        ul.appendChild(li);
+    }
+
+    const clearInfo = () => {
+        $('#moveset').empty();
+        $('#pokemon').empty();
+    }
+
+    clearInfo();
 
     $.ajax({
         url:'https://pokeapi.co/api/v2/pokemon/' + userInput.toLowerCase() + '/'
     }).then((data) => {
     
-
+        console.log(data);
         //check for undefined
         //if undefined, do not append
         //append an item to list
@@ -21,27 +42,49 @@ $('form').on('submit', (e)=>{
     let name = data.name;
     let nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
 
-    let pokeAttr = {
-        type1: data.types[0].type.name,
-        type2 : data.types[1].type.name,
-        move1 : data.moves[0].move.name,
-        move2 : data.moves[1].move.name,
-        move3 : data.moves[2].move.name,
-        move4 : data.moves[3].move.name
+    let pokeAttr = [
+        {
+            "moves" : {
+                move1 : "Move 1: " + data.moves[0].move.name,
+                move2 : "Move 2: " + data.moves[1].move.name,
+                move3 : "Move 3: " + data.moves[2].move.name,
+                move4 : "Move 4: " + data.moves[3].move.name
+            },
+
+            "types" : {
+                type1: "Type 1: " + data.types[0].type.name,
+                type2 : "Type 2: " + data.types[1].type.name
+            }
+        }
+    ]
+
+    const appendInfo = () => {
+
+        clearInfo();
+
+
+            Object.keys(pokeAttr[0].moves).forEach(name => {
+                if (pokeAttr[0].moves[name] !== undefined) {
+                    addMoves(pokeAttr[0].moves[name]);
+            }
+        }
+    );
+
+            Object.keys(pokeAttr[0].types).forEach(name => {
+                if (pokeAttr[0].types[name] !== undefined) {
+                 addPokeInfo(pokeAttr[0].types[name]);
+            }
+        }
+    );
+
+    $('.number').html("#"+data.id);
+    $('.name').html(nameCapitalized);
+
     }
 
-    console.log(pokeAttr.type1);
-
-        $('.number').html("#"+data.id);
-        $('.name').html(nameCapitalized);
-        $('.type1').html(data.types[0].type.name);
-        $('.type2').html(data.types[1].type.name);
-        $('.move1').html(data.moves[0].move.name);
-        $('.move2').html(data.moves[1].move.name);
-        $('.move3').html(data.moves[2].move.name);
-        $('.move4').html(data.moves[3].move.name);
-    },
-    (error) => {
-    console.log('bad request: ', error);
-    })
+    appendInfo();
+        },
+        (error) => {
+        console.log('bad request: ', error);
+        })
 })
